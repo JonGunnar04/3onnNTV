c3ad3d8b1d9a6c27e01480bf64ebfdfe0b36e2ae
JonGunnar04 <jon.gunnar.karlsson@gmail.com>
2026-02-24 20:59:54 +0000

    card

---
+++ NTV3onnVite/src/components/input.tsx
@@ -0,0 +1,17 @@
+
+
+export function Input({ value, onChange }: { value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        onChange(e); //e.target.value
+    }
+    return (
+        <div>
+            <input
+                type="text"
+                value={value}
+                onChange={(e) => handleChange(e)}
+
+            />
+        </div>
+    )
+}
